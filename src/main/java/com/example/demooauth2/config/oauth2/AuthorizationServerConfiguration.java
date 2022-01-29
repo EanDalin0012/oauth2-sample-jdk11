754b64b6bbc6ebe599b5791e7d313027db898679
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2022-01-29 13:02:44 +0700

    commit

--- src/main/java/com/example/demooauth2/config/oauth2/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/demooauth2/config/oauth2/AuthorizationServerConfiguration.java
@@ -1,13 +1,18 @@
 package com.example.demooauth2.config.oauth2;
 
+import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -15,8 +20,10 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -28,62 +35,51 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-	
-	/*
-	@Autowired
-	private UserDetailsService userService;
-	*/
-	
-	@Autowired
-	@Qualifier("authenticationManagerBean")
-	private AuthenticationManager authenticationManager;
-	
-	@Autowired
-	private DataSource oauthDataSource;
+
+    static org.apache.log4j.Logger log = Logger.getLogger(AuthorizationServerConfiguration.class.getName());
+
+    @Inject
+    private DataSource dataSource;
+    @Inject
+    private AuthenticationManager authenticationManager;
+
+    @Inject
+    private PasswordEncoder oauthClientPasswordEncoder;
+
 
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(oauthDataSource);
+        return new JdbcTokenStore(dataSource);
     }
 
     @Bean
-    public ApprovalStore approvalStore() {
-        return new JdbcApprovalStore(oauthDataSource);
+    public OAuth2AccessDeniedHandler oauthAccessDeniedHandler() {
+        return new OAuth2AccessDeniedHandler();
     }
 
-    @Bean
-    public AuthorizationCodeServices authorizationCodeServices() {
-        return new JdbcAuthorizationCodeServices(oauthDataSource);
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()").passwordEncoder(oauthClientPasswordEncoder);
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(oauthDataSource);
+        clients.jdbc(dataSource);
     }
 
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-    	oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(tokenStore());
+//        endpoints.userApprovalHandler(this.userApprovalHandler());
+        endpoints.authenticationManager(authenticationManager);
     }
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-    	TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        endpoints
-                .approvalStore(approvalStore())
-                .authenticationManager(authenticationManager)
-                .authorizationCodeServices(authorizationCodeServices())
-                .tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain)           
-                .authenticationManager(authenticationManager);
-    }
-    
     @Bean
-	@Primary
-	public DefaultTokenServices tokenServices() {
-		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-		defaultTokenServices.setTokenStore(tokenStore());
-		defaultTokenServices.setSupportRefreshToken(true);
-		return defaultTokenServices;
-	}
+    public ApprovalStore approvalStore() {
+        TokenApprovalStore store = new TokenApprovalStore();
+        store.setTokenStore(this.tokenStore());
+        return store;
+    }
 }
